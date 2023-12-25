c3014a23c4634baf0b9425e74dddb225f17fd185
fixren <2446821304@qq.com>
2023-12-25 15:59:40 +0800

    ...

---
+++ CanonGame/Assets/Scripts/CollisionWithProjectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionWithProjectile : MonoBehaviour {
+
+    public GameObject explosion;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(collision.gameObject, 1);
+            Instantiate(explosion, transform.position, Quaternion.identity);
+        }
+    }
+}
---
+++ CanonGame/Assets/Scripts/LaunchProjectile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchProjectile : MonoBehaviour {
+
+    public GameObject projectile;
+    public GameObject target;
+    float time;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        time += Time.deltaTime;
+
+        if(time >= 2.0)
+        {
+            time = 0;
+            transform.LookAt(target.transform);
+            GameObject t = Instantiate(projectile, transform.position, Quaternion.identity);           
+            t.GetComponent<Rigidbody>().AddForce(transform.forward * 2500);
+            Destroy(t, 3);
+        }
+
+       
+	}
+}
